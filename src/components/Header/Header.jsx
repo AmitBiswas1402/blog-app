d18e14fccec0e67bd8e6f7873426b00177be9905
Amit Biswas <amit.142biswas@gmail.com>
2024-05-24 22:38:40 +0530

    react

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {Conatiner, Logo, LogoutBtn} from '../index'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+  const authStatus = useSelector((state) => state.auth,status)
+
   return (
     <div>Header</div>
   )
