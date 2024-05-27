649c8c3bd1e0e38328c3757d0ab923ddf6f23172
Amit Biswas <amit.142biswas@gmail.com>
2024-05-27 21:15:11 +0530

    react

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { login as authLogin } from '../store/authSlice'
+import {Button} from 
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
