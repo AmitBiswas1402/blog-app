e561c484b5035465aa2952fb474b61efc67736ae
Amit Biswas <amit.142biswas@gmail.com>
2024-06-01 22:56:49 +0530

    react

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { Editor } from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
 
-export default function RTE() {
+export default function RTE({name, control, label, defaultValue=""}) {
   return (
-    <div>test</div>
+    <div className='w-full'>
+      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+      <Controller 
+      name='content'
+      />
+    </div>
   )
 }
 
