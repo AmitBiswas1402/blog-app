76726d8c0fa954ed8d41e5dd5079e249f0154bef
Amit Biswas <amit.142biswas@gmail.com>
2024-06-02 22:05:47 +0530

    react

---
+++ src/components/post-form/PostForm.jsx
@@ -0,0 +1,15 @@
+import React, {useCallback} from 'react'
+import { useForm } from 'react-hook-form'
+import {Button, Input, Select, RTE} from '../index'
+import aapwriteService from '../../appwrite/config'
+import { useNavigate } from 'react-router-dom'
+
+function PostForm() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PostForm
