b20f086f3cd88d6997da36bf4c4be3675702b5cc
Amit Biswas <amit.142biswas@gmail.com>
2024-06-03 22:59:26 +0530

    react

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -3,8 +3,19 @@ import { useForm } from 'react-hook-form'
 import {Button, Input, Select, RTE} from '../index'
 import aapwriteService from '../../appwrite/config'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function PostForm() {
+  const {register, handleSubmit, watch, setValue} = useForm({
+    defaultValues: {
+      defaultValues: {
+        title: '',
+        contents: '',
+        tags: [],
+      }
+    }
+  })
+
   return (
     <div>
 
