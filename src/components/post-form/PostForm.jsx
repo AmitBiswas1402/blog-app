45b37cd6f47ba6e16d9970310f710d76836124ae
Amit Biswas <amit.142biswas@gmail.com>
2024-06-04 17:32:35 +0530

    react

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,7 +1,7 @@
 import React, {useCallback} from 'react'
 import { useForm } from 'react-hook-form'
 import {Button, Input, Select, RTE} from '../index'
-import aapwriteService from '../../appwrite/config'
+import appwriteService from '../../appwrite/config'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
@@ -9,12 +9,26 @@ function PostForm() {
   const {register, handleSubmit, watch, setValue} = useForm({
     defaultValues: {
       defaultValues: {
-        title: '',
-        contents: '',
-        tags: [],
+        title: post?.title || '',
+        slug: post?.slug || '',
+        content: post?.content || '',
+        status: post?.status || 'active',
       }
     }
   })
+
+  const navigate = useNavigate()
+  const userDatae = useSelector(state => state.user.userData)
+
+  const submit = async (data) => {
+    if (post) {
+      const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null
+
+      if (file) {
+        appwriteService.deleteFile(post.image)
+      }
+    }
+  }
 
   return (
     <div>
