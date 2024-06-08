7510d7e5846cdb983d50d90bd3ec6f279e792dde
Amit Biswas <amit.142biswas@gmail.com>
2024-06-08 19:10:23 +0530

    react

---
+++ src/pages/AllPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Container, PostCard} from '../components'
+import appwriteService from '../appwrite/config'
+
+function AllPost() {
+  return (
+    <div>AllPost</div>
+  )
+}
+
+export default AllPost
